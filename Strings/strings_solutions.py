d1c19db5cad1fb03a8568c9d195b5c72d0e65eff
Carina Ogbat <carinawrightogbat@gmail.com>
2023-05-18 08:36:28 -0700

    added needle in haystack

--- Strings/strings_solutions.py
+++ Strings/strings_solutions.py
@@ -54,3 +54,17 @@ class Solution(object):
 
 #NOTES - to reverse a number you do need to convert it to a string to use slicing,
 #reverse() or sort() and sorted() methods.
+
+#28 FIND THE FIRST INDEX OF THE FIRST OCCURENCE IN A STRING
+#Given two strings needle and haystack, return the index of the first occurrence of needle 
+# in haystack, or -1 if needle is not part of haystack.
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+
+        return haystack.find(needle)
