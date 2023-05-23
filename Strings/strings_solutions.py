3f3e47e4ce251b3a8397b5d9eea49331ab021e37
Carina Ogbat <carinawrightogbat@gmail.com>
2023-05-22 20:26:34 -0700

    added length of last word solution

--- Strings/strings_solutions.py
+++ Strings/strings_solutions.py
@@ -68,3 +68,19 @@ class Solution(object):
         """
 
         return haystack.find(needle)
+
+#58 RETURN LENGTH OF LAST WORD
+# Given a string s consisting of words and spaces, return the length of the last word 
+# in the string.
+# A word is a maximal 
+# substring
+#  consisting of non-space characters only.
+
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        words = s.split()
+        return len(words[-1])
